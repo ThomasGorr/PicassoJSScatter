c095ba948644cee37c4e6d36efc63bde5c0dbdef
Thomas Gorr <thomas.gorr@inform-software.com>
2018-10-04 17:51:42 +0200

    Implements app.createCube for data management

--- src/component.js
+++ src/component.js
@@ -4,27 +4,51 @@ import {defineHTML} from "./lib/html/divDefinition";
 import picasso from "./../node_modules/picasso.js/dist/picasso";
 import picassoQ from "./../node_modules/picasso-plugin-q/dist/picasso-q";
 import {dompoint} from "./lib/picassoUtils/dompoint";
+import {myTooltip} from "./lib/picassoUtils/myTooltip";
 import {getUpdateConfig} from "./lib/picassoUtils/chartConfig";
+import qlik from "qlik";
 
-export function main($element, layout) {
+export async function main($element, layout, that) {
     try {
         picasso.use(picassoQ);
         picasso.component("dompoint", dompoint);
+        picasso.component("my-tooltip", myTooltip);
     } catch (e) {
         console.error(e);
     }
-    innerPaint($element, layout);
+    const app = qlik.currApp();
+    const properties = await that.backendApi.getProperties();
+    const data = await createCube(properties, app);
+    console.log("Properties", properties);
+    innerPaint($element, layout, data);
 }
 
-async function innerPaint($element, layout) {
-    defineHTML($, $element);
+async function createCube(properties, app) {
+    let qHyperCubeDef = properties.qHyperCubeDef;
+    if (properties.props.pointRepresentation === "calculatedIcon") {
+        qHyperCubeDef.qMeasures.push({
+            picassoScatters: {
+                technicalId: "pointRepresentation",
+            },
+            qDef: {
+                qDef: properties.props.calculatedItem,
+            },
+        });
+    }
+    console.log({qHyperCubeDef});
+    return await app.createCube(qHyperCubeDef);
+}
 
+async function innerPaint($element, layout, data) {
+    defineHTML($, $element);
+    console.log("Layout", layout);
+    console.log("Data", data);
     const pChart = picasso.chart({
         element: $element[0].querySelector("#scatter"),
         data: [],
     });
 
-    const updateConfig = getUpdateConfig(layout);
+    const updateConfig = getUpdateConfig(layout, data.layout.qHyperCube);
     pChart.update(updateConfig);
 
 }
