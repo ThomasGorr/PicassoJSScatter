e583d5ec0605e333c83461faca3f395954390cd7
Thomas Gorr <thomas.gorr@inform-software.com>
2018-10-04 13:39:46 +0200

    Adds advanced property definition for tooltip settings and appearance

--- src/definition.js
+++ src/definition.js
@@ -17,6 +17,64 @@ export const definition = {
         },
         appearance: {
             uses: "settings",
+            items: {
+                PointRepresentation: {
+                    type: "string",
+                    component: "dropdown",
+                    label: "Point representation",
+                    ref: "props.pointRepresentation",
+                    options: [
+                        {
+                            value: "point",
+                            label: "Point",
+                        },
+                        {
+                            value: "defaultIcon",
+                            label: "Default icon",
+                        },
+                        {
+                            value: "calculatedIcon",
+                            label: "Calculated icon",
+                        },
+                    ],
+                },
+                CalculatedItem: {
+                    type: "string",
+                    label: "Calculated icon",
+                    ref: "props.calculatedItem",
+                    show: (action) => {
+                        return action.props.pointRepresentation === "calculatedIcon";
+                    },
+                },
+            },
+        },
+        tooltip: {
+            type: "items",
+            label: "Tooltip definition",
+            items: {
+                tooltipList: {
+                    type: "array",
+                    ref: "tooltipArray",
+                    label: "Tooltip definition",
+                    itemTitleRef: "label",
+                    allowAdd: true,
+                    allowRemove: true,
+                    addTranslation: "Add Tooltip line",
+                    items: {
+                        key: {
+                            type: "string",
+                            ref: "props.tooltip.key",
+                            label: "Key",
+                        },
+                        value: {
+                            type: "string",
+                            ref: "props.tooltip.value",
+                            label: "Value",
+                            expression: "optional",
+                        },
+                    },
+                },
+            },
         },
     },
 };
